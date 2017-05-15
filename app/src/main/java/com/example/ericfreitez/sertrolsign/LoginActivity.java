03867565a74c9810dce1abdca343f7920b3b1a1e
Eric Freitez <ericf196@gmail.com>
2017-05-15 14:05:44 -0400

    Login v1

--- app/src/main/java/com/example/ericfreitez/sertrolsign/LoginActivity.java
+++ app/src/main/java/com/example/ericfreitez/sertrolsign/LoginActivity.java
@@ -1,27 +1,236 @@
 package com.example.ericfreitez.sertrolsign;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
-    Button btnLogin;
+public class LoginActivity extends AppCompatActivity /*implements View.OnClickListener*/{
+
+    private static final String DUMMY_USER_ID = "0000000000";
+    private static final String DUMMY_PASSWORD = "dummy_password";
+
+    private UserLoginTask mAuthTask = null;
+
+    private Button mFormButton;
+    private ImageView mLogoView;
+    private EditText mUserIdView;
+    private EditText mPasswordView;
+    private TextInputLayout mFloatLabelUserId;
+    private TextInputLayout mFloatLabelPassword;
+    private View mProgressView;
+    private View mLoginFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btnLogin= (Button) findViewById(R.id.btn_login);
 
-        btnLogin.setOnClickListener(this);
+        mLogoView = (ImageView) findViewById(R.id.image_logo);
+        mUserIdView = (EditText) findViewById(R.id.user_name);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mFloatLabelUserId = (TextInputLayout) findViewById(R.id.float_label_user_id);
+        mFloatLabelPassword = (TextInputLayout) findViewById(R.id.float_label_password);
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+        mFormButton= (Button) findViewById(R.id.form_button);
+
+        //mFormButton.setOnClickListener(this);
+
+        // Setup
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+
+                    if (!isOnline()) {
+                        showLoginError(getString(R.string.error_network));
+                        return false;
+                    }
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mFormButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!isOnline()) {
+                    showLoginError(getString(R.string.error_network));
+                    return;
+                }
+                attemptLogin();
+            }
+        });
 
     }
 
-    @Override
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+
+
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mFloatLabelUserId.setError(null);
+        mFloatLabelPassword.setError(null);
+
+        // Store values at the time of the login attempt.
+        String userId = mUserIdView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mFloatLabelPassword.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!isPasswordValid(password)) {
+        mFloatLabelPassword.setError(getString(R.string.error_invalid_password));
+        focusView = mFloatLabelPassword;
+        cancel = true;
+    }
+
+        // Verificar si el ID tiene contenido.
+        if (TextUtils.isEmpty(userId)) {
+            mFloatLabelUserId.setError(getString(R.string.error_field_required));
+            focusView = mFloatLabelUserId;
+            cancel = true;
+        } else if (!isUserIdValid(userId)) {
+            mFloatLabelUserId.setError(getString(R.string.error_field_required));
+            focusView = mFloatLabelUserId;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            mAuthTask = new UserLoginTask(userId, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private void showProgress(final boolean show) {
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+
+        int visibility = show ? View.GONE : View.VISIBLE;
+        mLogoView.setVisibility(visibility);
+        mLoginFormView.setVisibility(visibility);
+
+    }
+
+    /*@Override
     public void onClick(View v) {
         Intent mainActivity= new Intent(this, MainActivity.class);
         startActivity(mainActivity);
+    }*/
+
+    private boolean isUserIdValid(String userId) {
+        return userId.length() == 10;
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnected();
+    }
+
+    private void showLoginError(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+    }
+
+    public class UserLoginTask extends AsyncTask<Void, Void, Integer> {
+
+        private final String mUserId;
+        private final String mPassword;
+
+        UserLoginTask(String userId, String password) {
+            mUserId = userId;
+            mPassword = password;
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return 4;
+            }
+
+            if (!mUserId.equals(DUMMY_USER_ID)) {
+                return 2;
+            }
+
+            if (!mPassword.equals(DUMMY_PASSWORD)) {
+                return 3;
+            }
+
+            return 1;
+        }
+
+        @Override
+        protected void onPostExecute(final Integer success) {
+            mAuthTask = null;
+            showProgress(false);
+
+
+            switch (success) {
+                case 1:
+                    showMainActivity();
+                    break;
+                case 2:
+                case 3:
+                    showLoginError("Número de identificación o contraseña inválidos");
+                    break;
+                case 4:
+                    showLoginError(getString(R.string.error_server));
+                    break;
+            }
+
+
+        }
+
+        private void showMainActivity() {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
