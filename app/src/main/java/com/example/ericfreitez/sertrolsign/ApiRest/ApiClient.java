2e16d2f2427910c770e9cb82d72c07a25bc20250
Eric Freitez <ericf196@gmail.com>
2017-05-16 08:47:50 -0400

    agregado Retrofit y Gson en el Gradle y class ApiClient

---
+++ app/src/main/java/com/example/ericfreitez/sertrolsign/ApiRest/ApiClient.java
@@ -0,0 +1,25 @@
+package com.example.ericfreitez.sertrolsign.ApiRest;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Eric Freitez on 16/05/2017.
+ */
+
+public class ApiClient {
+
+    public static final String BASE_URL = "http://10.0.3.2/api/index.php/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(){
+        if (retrofit==null){
+            retrofit= new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit;
+    }
+
+
+}
